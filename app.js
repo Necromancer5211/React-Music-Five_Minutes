f0801462a3aa3c8ece8b846e35f0e43405dec54f
Necromancer5211 <genuine.basilnt@gmail.com>
2020-09-30 23:22:16 +0530

    Need to fix regenerator error

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon'); //eslint-disable-line
+const favicon = require("serve-favicon"); //eslint-disable-line
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -27,10 +27,9 @@ app.set('view engine', 'ejs');
 
 //  uncomment after placing your favicon in /public
 //  app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(expressSession);
 app.use(passport.initialize());
